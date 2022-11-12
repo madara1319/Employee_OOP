e42ac8dc8a426d22c90b356fd65642dbf416e7be
madara1319 <mateusz.bik@op.pl>
2022-11-12 16:58:13 +0100

    Added abstraction and inheritance example classes

--- Employee_OOP.cpp
+++ Employee_OOP.cpp
@@ -36,12 +36,22 @@ bool numberInString_checker(string &x)
 		}
 	}
 return a;
-}
+};
+//Below class is an Implementation of Abstraction
+//classes can inherit from this abstract class 
+//they have to sign contract defined within so provide implementation
+//for virtual class (here AskForPromotion)
+class AbstractEmployee
+{
+	virtual void AskForPromotion()=0;
+};
 
 
-class Employee {
+class Employee:AbstractEmployee {
 //private is by default in C++ so it is not needed
 	private:
+//These are private to get to them you need to go through methods
+//This is basically the concept of encapsulation
 		string Name;
 		string Company;
 		int Age;
@@ -85,16 +95,41 @@ class Employee {
 			std::cout<< "Company- " << Company << std::endl;
 			std::cout<< "Age - " << Age << std::endl;
 			
-		}
+		};
 //Creating a constructor, it does need to specify return type (like void) in front of, but it is a method just like IntroduceYourself, it needs to be named after class above it
 		Employee(string name, string company, int age)
 		{
 			Name=name;
 			Company=company;
 			Age=age;
+		};
+		void AskForPromotion()
+		{
+			if(Age>30)
+				std::cout<<Name<<" got promoted!" << std::endl;
+			else
+				std::cout<<Name<<", sorry NO promotion for you!"<<std::endl;
 		}
+
 //Creating your default constructor makes you lose your default constructor 
 };
+
+//Inheritance -> new class Developer inherits from class Employee
+class Developer:Employee
+{
+public:
+		string FavProgrammingLang;
+		Developer(string name, string company, int age, string favprogramminglang )
+			:Employee(name,company,age)
+		{
+			FavProgrammingLang=favprogramminglang;
+		};
+		void FixBug()
+		{
+			std::cout<<getName()<<" fixed bug using "<<FavProgrammingLang<<std::endl;
+		}
+};
+
 
 int main()
 
@@ -103,13 +138,19 @@ int main()
 //	employee1.IntroduceYourself();
 	Employee employee2=Employee("Szymon","Intel",26);
 //	employee2.IntroduceYourself();
-	employee1.setAge(15);
-	employee1.setName("Jacek");
-	employee2.setName("2Pawel");
-	std::cout<<employee2.getName()<<" is " <<employee2.getAge()<< " years old"<<std::endl;
-	std::cout<<employee1.getName()<<" is " <<employee1.getAge()<< " years old"<<std::endl;
-	string imie="Jacek";
-	std::cout<< numberInString_checker(imie);
+	employee1.setAge(39);
+//	employee1.setName("Jacek");
+//	employee2.setName("2Pawel");
+//	std::cout<<employee2.getName()<<" is " <<employee2.getAge()<< " years old"<<std::endl;
+//	std::cout<<employee1.getName()<<" is " <<employee1.getAge()<< " years old"<<std::endl;
+//	string imie="Jacek";
+//	std::cout<< numberInString_checker(imie);
+	employee1.AskForPromotion();
+	employee2.AskForPromotion();
+	Developer d=Developer("Maciek","YT",19,"C++");
+	d.FixBug();
+
+
 
 }
 
